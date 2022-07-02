32c5e4103969a031abc6e24b6aa816325cf46990
Lakshay-Singhal <lakshaysinghal08@gmail.com>
2022-07-02 23:48:47 +0530

    Props added

--- components/rightSidebar/EffectContent.tsx
+++ components/rightSidebar/EffectContent.tsx
@@ -1,7 +1,17 @@
+import { Flex, Input, Text } from 'native-base'
 import React from 'react'
 
 export default function EffectContent() {
     return (
-        <div>EffectContent</div>
+        <Flex direction='row' wrap="wrap">
+            <Flex direction='row' p={4}>
+                <Text> Opacity </Text>
+                <Input width={20} />
+            </Flex>
+            <Flex direction='row' p={4}>
+                <Text> Box shadow </Text>
+                <Input width={20} />
+            </Flex>
+        </Flex>
     )
 }
--- components/rightSidebar/TypographyContent.tsx
+++ components/rightSidebar/TypographyContent.tsx
@@ -1,7 +1,38 @@
+import { CheckIcon, Flex, Input, Select, Text } from 'native-base'
+import { BiAlignLeft, BiAlignMiddle, BiAlignRight, BiAlignJustify } from 'react-icons/bi';
 import React from 'react'
 
 export default function TypographyContent() {
     return (
-        <div>TypographyContent</div>
+        <Flex direction='row' wrap='wrap' justifyContent="space-around">
+                <Flex direction='row' p={2} >
+                    <Text> Style </Text>
+                    <button>B</button> 
+                    <button>I</button>
+                </Flex>
+                <Flex direction='row' p={2} >
+                    <Text> Text Align </Text>
+                    <button><BiAlignLeft/></button> 
+                    <button><BiAlignMiddle/></button>
+                    <button><BiAlignRight/></button>
+                    <button><BiAlignJustify/></button>
+                </Flex>
+                <Flex direction='row' p={2}>
+                    <Text> Font Size </Text>
+                    <Input width={20} />
+                </Flex>
+                <Flex direction='row' p={2}>
+                    <Text> Colour </Text>
+                    <Input width={20} />
+                </Flex>
+                <Flex direction='row' p={2}>
+                    <Text> Line height </Text>
+                    <Input width={20} />
+                </Flex>
+                <Flex direction='row' p={2}>
+                    <Text> Letter Spacing </Text>
+                    <Input width={20} />
+                </Flex>
+        </Flex>
     )
 }
