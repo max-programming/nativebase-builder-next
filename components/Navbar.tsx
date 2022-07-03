7d489b91fc4c83f39121b1e0e187c03b2b97cf27
Usman <ui.sabuwala@gmail.com>
2022-07-03 06:55:34 +0530

    Added Menu TSX and JSX save

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { Button, CloseIcon, Flex, HStack, Image, Text } from 'native-base';
+import {
+  Button,
+  CloseIcon,
+  Flex,
+  HStack,
+  Image,
+  Menu,
+  Text,
+} from 'native-base';
 import { saveAs } from 'file-saver';
 import { useGenerateFile } from '@/utils/useGenerateFile';
+import { JavaScriptIcon, TypeScriptIcon } from './Icons/TSandJSicons';
 
 const Navbar = () => {
   const { customCode, clearComponents } = useGenerateFile('CustomComponent');
 
-  const handleExportCode = () => {
-    const blob = new Blob([customCode], { type: 'text/typescriptreact' });
-    saveAs(blob, 'CustomCode.tsx');
+  const handleExportCode = (type: 'tsx' | 'jsx') => {
+    const blob = new Blob([customCode], { type: 'text/javascript' });
+    saveAs(blob, `CustomCode.${type}`);
   };
 
   return (
@@ -28,7 +37,8 @@ const Navbar = () => {
               width='55px'
               height='61px'
               marginTop={1}
-              src='https://www.notion.so/image/https%3A%2F%2Fs3.us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2388a952-7adb-40d8-9eaa-efdfbe8124be%2FUntitled.png%3FX-Amz-Algorithm%3DAWS4-HMAC-SHA256%26X-Amz-Content-Sha256%3DUNSIGNED-PAYLOAD%26X-Amz-Credential%3DAKIAT73L2G45EIPT3X45%252F20220628%252Fus-west-2%252Fs3%252Faws4_request%26X-Amz-Date%3D20220628T044127Z%26X-Amz-Expires%3D86400%26X-Amz-Signature%3D3eb578d58249796325eeaafd70547bc4f83f7ab849af01a559828f970e845033%26X-Amz-SignedHeaders%3Dhost%26x-id%3DGetObject?table=block&id=14a89eac-412c-4ea4-8dcb-c8bd2d7ff1f7&cache=v2'
+              src='https://docs.nativebase.io/_next/image?url=https%3A%2F%2Fdocs.nativebase.io%2Fimg%2Fnativebaselogo.svg&w=48&q=100'
+              alt='NativeBase Logo'
             />
             <Text
               marginLeft={2}
@@ -41,9 +51,30 @@ const Navbar = () => {
           </Flex>
         </HStack>
         <HStack space={5}>
-          <Button colorScheme='coolGray' onPress={handleExportCode}>
-            Export Code
-          </Button>
+          <Menu
+            bg='coolGray.700'
+            placement='bottom right'
+            trigger={triggerProps => (
+              <Button colorScheme='coolGray' {...triggerProps}>
+                Export Code
+              </Button>
+            )}
+          >
+            <Menu.Item
+              bg='coolGray.700'
+              _hover={{ bg: 'coolGray.600' }}
+              onPress={() => handleExportCode('jsx')}
+            >
+              <JavaScriptIcon />
+            </Menu.Item>
+            <Menu.Item
+              bg='coolGray.700'
+              _hover={{ bg: 'coolGray.600' }}
+              onPress={() => handleExportCode('tsx')}
+            >
+              <TypeScriptIcon />
+            </Menu.Item>
+          </Menu>
           <Button
             colorScheme='coolGray'
             leftIcon={<CloseIcon />}
